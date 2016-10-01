507851169ad36041df4e4359b7ce0d0362714cf8
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2016-10-01 11:33:45 -0500

    Finished protocol detection code.

--- http2.js
+++ http2.js
@@ -10,7 +10,8 @@ var server = http2.createServer({
 	cert: fs.readFileSync(path.join(__dirname, "/crt/localhost.crt"))
 }, function(request, response){
 	var filename = path.join(pubDir, request.url),
-		contentType = mime.lookup(filename);
+		contentType = mime.lookup(filename),
+		protocolVersion = request.isSpdy ? "http2" : "http1";
 
 	if((filename.indexOf(pubDir) === 0) && fs.existsSync(filename) && fs.statSync(filename).isFile()){
 		response.writeHead(200, {
@@ -18,9 +19,32 @@ var server = http2.createServer({
 			"cache-control": "max-age=3600"
 		});
 
-		var fileStream = fs.createReadStream(filename);
-		fileStream.pipe(response);
-		fileStream.on("finish", response.end);
+		if(protocolVersion === "http1" && filename.indexOf(".html") !== -1){
+			fs.readFile(filename, function(error, data){
+				jsdom.env(data.toString(), function(error, window){
+					window.document.documentElement.classList.add(protocolVersion);
+
+					var scripts = window.document.querySelectorAll("script:not([crossorigin])"),
+						jQueryScript = window.document.querySelector("script[crossorigin]"),
+						concatenatedScript = window.document.createElement("script");
+						concatenatedScript.src = "js/scripts.min.js";
+
+					for(var i in scripts){
+						scripts[i].remove();
+					}
+
+					jQueryScript.parentNode.insertBefore(concatenatedScript, jQueryScript.nextSibling);
+
+					var newDocument = "<!doctype html>" + window.document.documentElement.outerHTML;
+					response.end(newDocument);
+				});
+			});
+		}
+		else{
+			var fileStream = fs.createReadStream(filename);
+			fileStream.pipe(response);
+			fileStream.on("finish", response.end);
+		}
 	}
 	else{
 		response.writeHead(404);
